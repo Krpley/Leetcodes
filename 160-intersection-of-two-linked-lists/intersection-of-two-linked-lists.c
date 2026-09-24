6ffe952067b47dbc1705dcc8c1a4975edc5e61ca
Kyle Pasta <120144201+Krpley@users.noreply.github.com>
2026-09-24 13:50:03 -0400

    Time: 66 ms (52.27%) | Memory: 23.4 MB (53.87%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.c
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.c
@@ -12,21 +12,57 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
     struct ListNode* intersect = NULL;
     int skipA = 0;
     int skipB = 0;
-    while (listA != NULL) {
-        listB = headB;
-        if (listA == listB) {
+    int aCount = 0;
+    int bCount = 0;
+    int countDiff;
+
+    //Perform a double walk of both lists to count how long each list is
+    while (listA != NULL || listB != NULL) {
+        //Check for intersection
+        if (listA == listB && listA != NULL) {
             return listA;
         }
-        while (listB != NULL) {
-            if (listA == listB) {
-                return listA;
-            } else {
-                skipB ++;
-            }
+        //Move onto next pointers and increase count, if one is null stop counting
+        if (listA != NULL) {
+            listA = listA->next;
+            aCount++;
+            skipA ++;
+        }
+        if (listB != NULL) {
+            listB = listB->next;
+            bCount ++;
+            skipB ++;
+        }
+    }
+
+    //reset variables
+    skipA = 0;
+    skipB = 0;
+    listA = headA;
+    listB = headB;
+
+    if (aCount >= bCount) {
+        aCount -= bCount;
+        for (int i = 0; i < aCount; i++) {
+            listA = listA->next;
+        }
+    } else {
+        bCount -= aCount;
+        for (int i = 0; i < bCount; i++) {
             listB = listB->next;
         }
-        listA = listA->next;
-        skipA ++;
     }
+
+    while (listA != NULL && listB != NULL) {
+        if (listA == listB) {
+            return listA;
+        } else {
+            skipA ++;
+            skipB++;
+            listA = listA->next;
+            listB = listB->next;
+        }
+    }
+
     return NULL;
 }
