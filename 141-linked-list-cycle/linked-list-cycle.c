e1a652da315ba379fd5df926aa0ca20e8e742012
Kyle Pasta <120144201+Krpley@users.noreply.github.com>
2026-09-25 13:54:04 -0400

    Time: 7 ms (91.15%) | Memory: 12 MB (47.22%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.c
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -6,11 +6,16 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
+    //Create two nodes to go through the list at different speeds
     struct ListNode* fast = head;
     struct ListNode* slow = head;
+    //Handle possible null and single lists with while statement
     while (fast != NULL && fast->next != NULL) {
+        //Move slow up by 1
         slow = slow->next;
+        //Move fast up by 2
         fast = fast->next->next;
+        //if they have a similar memory address then there must be a cycle
         if (slow == fast) {
             return true;
         }
