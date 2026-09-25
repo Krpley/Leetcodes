15280b775eaff6e31baeb25e4307bc8898d07049
Kyle Pasta <120144201+Krpley@users.noreply.github.com>
2026-09-25 13:51:39 -0400

    Time: 13 ms (34.97%) | Memory: 12.1 MB (7.87%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.c
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -8,17 +8,11 @@
 bool hasCycle(struct ListNode *head) {
     struct ListNode* fast = head;
     struct ListNode* slow = head;
-    if (slow == NULL) {
-        return false;
-    }
-    while (fast->next != NULL) {
-        fast = fast->next;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
         if (slow == fast) {
             return true;
-        }
-        slow = slow->next;
-        if (fast->next != NULL) {
-            fast = fast->next;
         }
     }
     return false;
